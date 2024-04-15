b94ac1bbb2c1fe34e06c0416cc21b7d92faf4d13
amra-m <amahmutovic2@live.maryville.edu>
2024-04-15 12:11:59 -0500

    commit

--- studereApp/HomeViewController.swift
+++ studereApp/HomeViewController.swift
@@ -5,7 +5,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
--- studereApp/SignUpViewController.swift
+++ studereApp/SignUpViewController.swift
@@ -13,7 +13,6 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     @IBAction func signUpButtonTapped(_ sender: UIButton) {
     }
