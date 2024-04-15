70b486ceb7f7e1f04bbc6c4880a7b83003b2d358
Amra Mahmutovic <amahmutovic2@live.maryville.edu>
2024-04-15 08:48:21 -0500

    Initial Commit

---
+++ studereApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
---
+++ studereApp/SignUpViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameTextField: UIImageView!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    @IBOutlet weak var signupButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signUpButtonTapped(_ sender: UIButton) {
+    }
+    
+}
